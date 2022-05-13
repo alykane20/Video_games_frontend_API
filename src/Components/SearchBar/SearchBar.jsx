b28c1f7d44fd12af0cb4bf0bb027383ad040dd5c
alykane20 <kane.aly@gmail.com>
2022-05-13 10:46:36 -0400

    Created SearchBar component + started SearchBar functionality

---
+++ src/Components/SearchBar/SearchBar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+const SearchBar = (props) => {
+
+
+    return (
+        <form>
+            <input type="search"/> 
+            <button type="submit">Search</button>
+
+            
+
+
+        </form>
+      );
+}
+ 
+export default SearchBar;
